f2bf087fce18f3941f098c740cd0c3645fee21b2
MFA571 <fakhar571@gmail.com>
2019-08-03 23:24:17 +0500

    added skeleton code

---
+++ main.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+import pygame
+import random
+import math
+from tkinter import messagebox
+
+
+class Cube(object):
+    rows = 0
+    width = 0
+
+    def __init__(self, start, direction_x=1, direction_y=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, direction_x, direction_y):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class Snake(object):
+    def __init__(self, color, position):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def add_cube(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+def draw_grid(width_, rows_, surface):
+    pass
+
+
+def redraw_window(surface):
+    pass
+
+
+def message_box(subject, content):
+    pass
+
+
+def main():
+    pass
+
+
+rows = 0
+width = 0
+height = 0
+
+Cube.rows = rows
+Cube.width = width
+
+main()
+
