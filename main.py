55a1e8355dedba1a5c379e8ecf09d7ffc7e7e92b
MFA571 <fakhar571@gmail.com>
2019-08-04 21:14:09 +0500

    added directions..

--- main.py
+++ main.py
@@ -20,11 +20,52 @@ class Cube(object):
 
 
 class Snake(object):
+    body = []
+    turns = {}
+
     def __init__(self, color, position):
-        pass
+        self.color = color
+        self.head = Cube(position)  # new snake contains just a head
+        self.body.append(self.head)
+        self.direction_x = 0
+        self.direction_y = 1
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.direction_x = -1
+                    self.direction_y = 0
+                    # current position of head of our snake should be stored in order to move tails towards that dir
+                    self.turns[self.head.positon[:]] = [self.direction_x, self.direction_y]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.direction_x = 1
+                    self.direction_y = 0
+                    self.turns[self.head.positon[:]] = [self.direction_x, self.direction_y]
+
+                elif keys[pygame.K_UP]:
+                    self.direction_x = 0
+                    self.direction_y = -1
+                    self.turns[self.head.positon[:]] = [self.direction_x, self.direction_y]
+
+                elif keys[pygame.K_DOWN]:
+                    self.direction_x = 0
+                    self.direction_y = 1
+                    self.turns[self.head.positon[:]] = [self.direction_x, self.direction_y]
+
+        for i, c in enumerate(self.body):
+            p = c.position[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
 
     def reset(self):
         pass
